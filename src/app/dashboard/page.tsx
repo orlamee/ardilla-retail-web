a75d7e3a78f27dfc16c4a7be85c29b0a70254f05
orlamee <meetmichaelakindele@gmail.com>
2024-01-31 12:25:04 +0100

    fixes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,17 +2,17 @@
 import NavBar from "@/components/navbar";
 import React from "react";
 import Image from "next/image";
-import groupavatar from "../../assets/group avatar.png";
+import groupavatar from "../../assets/groupie.svg";
 import calc from "../../assets/cal.svg";
+import savefund from "../../assets/securefund.svg";
 import plane from "../../assets/plane.svg";
+import league from "../../assets/league.svg";
 import withdraw from "../../assets/withdraw.svg";
 import deposit from "../../assets/deposit.svg";
 import Hero from "@/components/dashboard/hero";
 import Quickaction from "./quickaction";
 import TodoGroup from "@/components/dashboard/todoGroup";
 import Buildwealth from "@/components/dashboard/buildwealth";
-// import Buildwealth from "@/components/dashboard/buildwealth";
-// import Hero from "@/components/dashboard/hero";
 function Dashboard() {
   return (
     <section>
@@ -36,6 +36,116 @@ function Dashboard() {
             <Image src={calc} alt="calc" className="ml-auto mt-[-50px] mr-[40px]" />
           </div>
           <Buildwealth/>
+          <div className="flex flex-col md:flex-row md:justify-between flex-wrap gap-[25px] md:gap-[40px]">
+            <div className="flex-1 h-full w-full">
+              <div className="p-10 h-full w-full flex justify-between items-center flex-1 border-[2px] border-[#000] bg-[#FCB40C] rounded-[12px] shadow-[0px_4px_30px_0px_#9494941A]">
+                <div>
+                  <h1 className="text-[17px] font-[400] text-center mb-1">
+                    Find Your Fellow
+                  </h1>
+                  <button className="rounded-full w-full font-[600] flex justify-center items-center px-8 py-3 bg-white text-black shadow-[2px_3px_black]">
+                    Ardilla User
+                  </button>
+                </div>
+                <Image src={plane} width={90} height={30} alt="plane" />
+                <div>
+                  <p className="font-[500] text-[16px] text-[#000]">
+                    Send money to KodHex
+                  </p>
+                  <Image
+                    src={groupavatar}
+                    alt="group users"
+                    className="mx-auto mt-3"
+                  />
+                </div>
+              </div>
+              <Image src={savefund} alt="savefund" className="w-full" />
+              <Image src={league} alt="league" className="w-full" />
+            </div>
+            <div className="flex-1 p-8 rounded-[12px] border-[1px] bg-white border-borderprimary shadow-[0px_4px_30px_0px_#9494941A]">
+              <div className="flex justify-between mb-6">
+                <h1 className="font-[500] text-[14px] text-gray-400">
+                  Transaction History
+                </h1>
+                <p className="text-[12px] font-[500] text-primary">View More</p>
+              </div>
+              <div className="flex justify-between items-center rounded-[5px] bg-[#F9FAFB] mb-4 p-5">
+                <div className="flex items-center">
+                  <Image src={withdraw} width={40} height={40} alt="witdraw" />
+
+                  <div className="ml-4">
+                    <h1 className="text-[14px] font-[500] mb-[6px]">
+                      Send Money to Lekan
+                    </h1>
+                    <p className="text-gray400 font-[400] text-[11px]">
+                      Tue, 22nd December, 2023 09:40 PM
+                    </p>
+                  </div>
+                </div>
+                <div className="text-[16px] font-[500]">-₦ 4,000.00 </div>
+              </div>
+              <div className="flex justify-between items-center rounded-[5px] bg-[#F9FAFB] mb-4 p-5">
+                <div className="flex items-center">
+                  <Image src={deposit} width={40} height={40} alt="witdraw" />
+
+                  <div className="ml-4">
+                    <h1 className="text-[14px] font-[500] mb-[6px]">
+                      Received Money from Daniel
+                    </h1>
+                    <p className="text-gray400 font-[400] text-[11px]">
+                      Tue, 22nd December, 2023 09:40 PM
+                    </p>
+                  </div>
+                </div>
+                <div className="text-[16px] font-[500]">+₦ 4,000.00 </div>
+              </div>
+              <div className="flex justify-between items-center rounded-[5px] bg-[#F9FAFB] mb-4 p-5">
+                <div className="flex items-center">
+                  <Image src={withdraw} width={40} height={40} alt="witdraw" />
+
+                  <div className="ml-4">
+                    <h1 className="text-[14px] font-[500] mb-[6px]">
+                      Send Money to Lekan
+                    </h1>
+                    <p className="text-gray400 font-[400] text-[11px]">
+                      Tue, 22nd December, 2023 09:40 PM
+                    </p>
+                  </div>
+                </div>
+                <div className="text-[16px] font-[500]">-₦ 4,000.00 </div>
+              </div>
+              <div className="flex justify-between items-center rounded-[5px] bg-[#F9FAFB] mb-4 p-5">
+                <div className="flex items-center">
+                  <Image src={deposit} width={40} height={40} alt="witdraw" />
+
+                  <div className="ml-4">
+                    <h1 className="text-[14px] font-[500] mb-[6px]">
+                      Received Money from Daniel
+                    </h1>
+                    <p className="text-gray400 font-[400] text-[11px]">
+                      Tue, 22nd December, 2023 09:40 PM
+                    </p>
+                  </div>
+                </div>
+                <div className="text-[16px] font-[500]">+₦ 4,000.00 </div>
+              </div>
+              <div className="flex justify-between items-center rounded-[5px] bg-[#F9FAFB] mb-4 p-5">
+                <div className="flex items-center">
+                  <Image src={deposit} width={40} height={40} alt="witdraw" />
+
+                  <div className="ml-4">
+                    <h1 className="text-[14px] font-[500] mb-[6px]">
+                      Received Money from Daniel
+                    </h1>
+                    <p className="text-gray400 font-[400] text-[11px]">
+                      Tue, 22nd December, 2023 09:40 PM
+                    </p>
+                  </div>
+                </div>
+                <div className="text-[16px] font-[500]">+₦ 4,000.00 </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </section>
--- src/components/dashboard/hero.tsx
+++ src/components/dashboard/hero.tsx
@@ -8,9 +8,9 @@ import Image from "next/image";
 function Hero() {
   const [hideBalance, setHideBalance] = useState(false);
   return (
-    <div className="dashboard-hero my-[50px] h-[250px] rounded-[16px] flex flex-wrap justify-between bg-primary items-center px-10 relative">
+    <div className="dashboard-hero my-[50px] h-[250px] rounded-[16px] flex flex-wrap justify-between items-center px-10">
       <div>
-        <h1 className=" text-white font-[500] text-[14px] flex">
+        <h1 className=" text-black font-[500] text-[14px] flex">
           Wallet Balance{" "}
           <span
             className="ml-4 cursor-pointer"
@@ -19,11 +19,11 @@ function Hero() {
             {hideBalance ? <Eye /> : <EyeOff />}
           </span>
         </h1>
-        <h2 className="font-[700] text-white text-[22px] md:text-[35px] lg:text-[50px] mb-5">
+        <h2 className="font-[700] text-[#3D0072] text-[22px] md:text-[35px] lg:text-[50px] mb-5">
           {hideBalance ? "*******" : " N 10,000,000.00"}
         </h2>
         <Link href={"/dashboard"}>
-          <div className="text-white text-[12px] font-[500] flex">
+          <div className="text-black text-[12px] font-[500] flex">
             View Account Details{" "}
             <span className="ml-3">
               <Image
@@ -36,15 +36,9 @@ function Hero() {
           </div>
         </Link>
       </div>
-      <button className="bg-white text-primary font-[700] text-[12px] py-5 px-10 rounded-full lg:px-[100px] 2xl:px-[130px]">
+      <button className="bg-[#3D0072] text-white font-[700] text-[12px] py-5 px-10 rounded-full lg:px-[100px] 2xl:px-[130px]">
         Add Money
       </button>
-      <div className="absolute text-white bottom-[-20px] z-[-1] bg-[#D2B7FB] rounded-[16px] w-[97%] mx-auto inset-0 h-[262px] overflow-hidden">
-        hello world
-      </div>
-      <div className="absolute text-white bottom-[-90px] z-[-2] bg-[#F0E7FE] rounded-[16px] w-[94%] mx-auto inset-0 h-[273px] overflow-hidden">
-        hello world
-      </div>
     </div>
   );
 }
