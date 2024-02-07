1df46f5f7c6c06cd32bd914384fa83fa094ace28
orlamee <meetmichaelakindele@gmail.com>
2024-02-07 15:25:20 +0100

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = {
+const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
-}
+};
 
 
 export default nextConfig;
 
-
+// If using ES6 modules, you can use the following syntax instead of module.exports
+// export default nextConfig;
